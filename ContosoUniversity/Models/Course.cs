a458aff126ffa3ec60c6fbb68da98237de65a220
振豪 林 <k477522@yahoo.com.tw>
2020-11-08 21:02:18 +0800

    Upload

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    /// <summary>
+    /// 課程
+    /// </summary>
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    /// <summary>
+    /// 年級
+    /// </summary>
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    /// <summary>
+    /// 註冊
+    /// </summary>
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
